1f99212a7bb4cd31d48c86d1b292d31dfea37c9a
Dennis Chan <dennisming2012@gmail.com>
2021-11-17 15:09:03 +0800

    added seed

--- seeds/00-delete-tables.ts
+++ seeds/00-delete-tables.ts
@@ -5,6 +5,6 @@ const foodTableName = "food";
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
-  await knex(userTableName).del();
-  await knex(foodTableName).del();
+  await knex.raw(`TRUNCATE ${userTableName} RESTART IDENTITY CASCADE`);
+  await knex.raw(`TRUNCATE ${foodTableName} RESTART IDENTITY CASCADE`);
 }
---
+++ seeds/01-init-data.ts
@@ -0,0 +1,107 @@
+import { Knex } from "knex";
+import { hashPassword } from "../utils/hash";
+
+const userTableName = "users";
+const foodTableName = "food";
+
+const users = [
+  {
+    username: "billy",
+    password: "1234",
+    gender: "male",
+    age: 40,
+    height: 165,
+    weight: 80.5,
+  },
+  {
+    username: "dennis",
+    password: "1234",
+    gender: "male",
+    age: 18,
+    height: 175,
+    weight: 64,
+  },
+  {
+    username: "matthew",
+    password: "1234",
+    gender: "male",
+    age: 25,
+    height: 173,
+    weight: 58,
+  },
+  {
+    username: "maggie",
+    password: "1234",
+    gender: "female",
+    age: 30,
+    height: 190,
+    weight: 72,
+  },
+];
+
+const food = [
+  {
+    food_name: "魚仔餅",
+    food_photo: "1.jpg",
+    energy: 500,
+    protein: 10.8,
+    total_fat: 4.7,
+    saturated_fat: 5,
+    trans_fat: 5.5,
+    carbohydrates: 52.3,
+    sodium: 230,
+    user_id: 1,
+  },
+  {
+    food_name: "可口可樂",
+    food_photo: "2.jpg",
+    energy: 500,
+    protein: 10.8,
+    total_fat: 4.7,
+    saturated_fat: 5,
+    trans_fat: 5.5,
+    carbohydrates: 52.3,
+    sodium: 230,
+    user_id: 3,
+  },
+  {
+    food_name: "益達無糖香口膠",
+    food_photo: "3.jpg",
+    energy: 500,
+    protein: 10.8,
+    total_fat: 4.7,
+    saturated_fat: 5,
+    trans_fat: 5.5,
+    carbohydrates: 52.3,
+    sodium: 230,
+    user_id: 4,
+  },
+  {
+    food_name: "金莎朱古力",
+    food_photo: "4.jpg",
+    energy: 500,
+    protein: 10.8,
+    total_fat: 4.7,
+    saturated_fat: 5,
+    trans_fat: 5.5,
+    carbohydrates: 52.3,
+    sodium: 230,
+    user_id: 4,
+  },
+];
+
+if (0) {
+  console.log(food, foodTableName);
+}
+
+export async function seed(knex: Knex): Promise<void> {
+  const userData = await Promise.all(
+    users.map(async (user) => ({
+      ...user,
+      password: await hashPassword(user.password),
+    }))
+  );
+
+  await knex(userTableName).insert(userData);
+  await knex(foodTableName).insert(food);
+}
