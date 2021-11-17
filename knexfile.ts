70d8050413baba0ba657f95bd2f74c1551f38df9
Dennis Chan <dennisming2012@gmail.com>
2021-11-17 10:12:54 +0800

    added .env.example and test db

--- knexfile.ts
+++ knexfile.ts
@@ -48,4 +48,21 @@ module.exports = {
       tableName: "knex_migrations",
     },
   },
+
+  test: {
+    client: "postgresql",
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
