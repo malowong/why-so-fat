0fc19a2a81f6a2702c32e2493e7f886b8aee0761
Dennis Chan <dennisming2012@gmail.com>
2021-11-17 15:58:34 +0800

    added guards.ts

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import http from "http";
 import Knex from "knex";
 import path from "path";
 import { Server as SocketIO } from "socket.io";
+import { isLoggedIn } from "./utils/guards";
 import { logger } from "./utils/logger";
 
 const app = express();
@@ -14,10 +15,11 @@ export const knex = Knex(knexConfig[process.env.NODE_ENV || "development"]);
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "private")));
+app.use(isLoggedIn, express.static(path.join(__dirname, "public")));
 
-const API_VERSION = "/api";
-app.use(API_VERSION, routes);
+// const API_VERSION = "/api";
+// app.use(API_VERSION, routes);
 
 const PORT = 8080;
 server.listen(PORT, () => {
