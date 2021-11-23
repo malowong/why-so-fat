a2fb87f3f3a92727199de044729ef61d1b2c95e1
Billy <billychu25@gmail.com>
2021-11-23 15:15:49 +0800

    added upload fnc

--- web-server/private/js/upload.js
+++ web-server/private/js/upload.js
@@ -4,12 +4,19 @@ form.addEventListener('submit', async (e) => {
     e.preventDefault()
     const formData = new FormData()
     formData.append('food_name', form.food_name.value)
+    formData.append('per_unit', form.per_unit.value)
     formData.append('total_weight', form.total_weight.value)
+    formData.append('serving_size', form.serving_size.value)
     formData.append('image', form.image.files[0])
-    console.log({
-        a: form.food_name.value,
-        b: form.image.files[0],
-    })
+    formData.append('energy', form.energy.value)
+    formData.append('protein', form.protein.value)
+    formData.append('total_fat', form.total_fat.value)
+    formData.append('saturated_fat', form.saturated_fat.value)
+    formData.append('trans_fat', form.trans_fat.value)
+    formData.append('carbohydrates', form.carbohydrates.value)
+    formData.append('sugars', form.sugars.value)
+    formData.append('sodium', form.sodium.value)
+    console.log(formData)
     form.reset()
     document.getElementById('blah').removeAttribute('src')
     const resp = await fetch('/api/food/upload', {
@@ -20,5 +27,15 @@ form.addEventListener('submit', async (e) => {
         console.log('OK')
     } else {
         console.log('error')
+    }
+})
+
+document.querySelector('#per_unit').addEventListener('click', () => {
+    if (form.per_unit.value == 'per_serving') {
+        document.getElementById('serving_size').hidden = false
+    } else if (form.per_unit.value == 'per_package') {
+        document.getElementById('serving_size').hidden = true
+    } else {
+        document.getElementById('serving_size').hidden = true
     }
 })
