7f8b78c62506c641cb3ea19f2814e1094035daba
Matthew Wong <matthew.w1996@hotmail.com>
2021-11-29 20:16:02 +0800

    python

--- web-server/routers/foodRoutes.ts
+++ web-server/routers/foodRoutes.ts
@@ -16,10 +16,10 @@ const storage = multer.diskStorage({
     cb(null, `${file.fieldname}-${Date.now()}.${file.mimetype.split("/")[1]}`);
   },
 });
-const upload = multer({ storage });
+const upload = multer({ storage, limits: { fileSize: 10000000 } });
 
 const ocrStorage = multer.memoryStorage();
-const ocrUpload = multer({ storage: ocrStorage });
+const ocrUpload = multer({ storage: ocrStorage, limits: { fileSize: 10000000 }});
 
 export const foodRoutes = express.Router();
 
